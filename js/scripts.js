b850260f51012ea04b3d25387d548cf034204ca5
Melissa <melissa.scmi@gmail.com>
2021-08-27 14:15:32 -0700

    add additional toppings the easy /not-dry way

--- js/scripts.js
+++ js/scripts.js
@@ -89,9 +89,12 @@ friendPizza.theDamage();
 
 // Business Logic for Pizza ---------
 
-function Pizza(SizeChoice, ToppingChoice) {
+function Pizza(SizeChoice, ToppingChoice, ToppingChoice2, ToppingChoice3, ToppingChoice4) {
   this.size = SizeChoice;
   this.topping = ToppingChoice;
+  this.topping2 = ToppingChoice2;
+  this.topping3 = ToppingChoice3;
+  this.topping4 = ToppingChoice4;
   this.newToppings = {};
 }
 
@@ -104,7 +107,29 @@ const sizes = [
 const toppings = [
   {topping: "Ice Cream", price: 3},
   {topping: "Happy Egg", price: 2},
-  {topping: "A Kitten", price: 10}
+  {topping: "A Kitten", price: 10},
+  {topping: "Cute Little Clouds", price: 15}
+]
+
+const toppings2 = [
+  {topping2: "Ice Cream", price: 3},
+  {topping2: "Happy Egg", price: 2},
+  {topping2: "A Kitten", price: 10},
+  {topping2: "Cute Little Clouds", price: 15}
+]
+
+const toppings3 = [
+  {topping3: "Ice Cream", price: 3},
+  {topping3: "Happy Egg", price: 2},
+  {topping3: "A Kitten", price: 10},
+  {topping3: "Cute Little Clouds", price: 15}
+]
+
+const toppings4 = [
+  {topping4: "Ice Cream", price: 3},
+  {topping4: "Happy Egg", price: 2},
+  {topping4: "A Kitten", price: 10},
+  {topping4: "Cute Little Clouds", price: 15}
 ]
 
 Pizza.prototype.addPlusToppings = function(newTopping) {
@@ -125,6 +150,24 @@ Pizza.prototype.theDamage = function () {
       cost += toppings[i].price;
     }
   }
+  for (let i = 0;  i < toppings2.length; i++) {
+    const yourPizza = toppings2[i].topping2;
+    if (yourPizza === this.topping2) {
+      cost += toppings2[i].price;
+    }
+  }
+  for (let i = 0;  i < toppings3.length; i++) {
+    const yourPizza = toppings3[i].topping3;
+    if (yourPizza === this.topping3) {
+      cost += toppings3[i].price;
+    }
+  }
+  for (let i = 0;  i < toppings4.length; i++) {
+    const yourPizza = toppings4[i].topping4;
+    if (yourPizza === this.topping4) {
+      cost += toppings4[i].price;
+    }
+  }
     return cost;
 }
 
@@ -137,9 +180,25 @@ function PlusToppings(name, addTopPrice) {
 
 // Test Content -----------
 
-let friendPizza = new Pizza("itty", "Happy Egg");
+let friendPizza = new Pizza("itty", "Happy Egg", "Ice Cream");
 let sadEgg = new PlusToppings("Sad Egg", 7);
 friendPizza.addPlusToppings(sadEgg);
 friendPizza;
 
 friendPizza.theDamage();
+
+// --------------------------
+
+// PlusToppings.prototype.sumToppings = function () {
+//   let cost = 0;
+//   for (let i = 0;  i < names.length; i++) {
+//     const yourToppings = names[i].name;
+//     if (yourToppings === this.name) {
+//       cost += names[i].addTopPrice;
+//     }
+//   }
+//     return cost;
+// }
+
+// UI
+
