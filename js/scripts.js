b59a65d75c597e9273c00a4f0c8825e188145728
Melissa <melissa.scmi@gmail.com>
2021-08-27 18:22:18 -0700

    clear dropdowns & related logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
 
 // Business Logic for Pizza ---------
 
-function Pizza(SizeChoice, ToppingChoice, ToppingChoice2, ToppingChoice3, ToppingChoice4) {
+function Pizza(SizeChoice, ToppingChoice) {
   this.size = SizeChoice;
   this.topping = ToppingChoice;
-  this.topping2 = ToppingChoice2;
-  this.topping3 = ToppingChoice3;
-  this.topping4 = ToppingChoice4;
-  this.newToppings = {};
 }
 
 const sizes = [
@@ -17,31 +13,10 @@ const sizes = [
 ]
 
 const toppings = [
-  {topping: "Ice Cream", price: 3},
-  {topping: "Happy Egg", price: 2},
-  {topping: "A Kitten", price: 10},
-  {topping: "Cute Little Clouds", price: 15}
-]
-
-const toppings2 = [
-  {topping2: "Ice Cream", price: 3},
-  {topping2: "Happy Egg", price: 2},
-  {topping2: "A Kitten", price: 10},
-  {topping2: "Cute Little Clouds", price: 15}
-]
-
-const toppings3 = [
-  {topping3: "Ice Cream", price: 3},
-  {topping3: "Happy Egg", price: 2},
-  {topping3: "A Kitten", price: 10},
-  {topping3: "Cute Little Clouds", price: 15}
-]
-
-const toppings4 = [
-  {topping4: "Ice Cream", price: 3},
-  {topping4: "Happy Egg", price: 2},
-  {topping4: "A Kitten", price: 10},
-  {topping4: "Cute Little Clouds", price: 15}
+  {topping: "Chocolate Frosting", price: 3},
+  {topping: "Sunshine", price: 10},
+  {topping: "Love", price: 0},
+  {topping: "Marinara", price: 3}
 ]
 
 Pizza.prototype.theDamage = function () {
@@ -56,24 +31,6 @@ Pizza.prototype.theDamage = function () {
     const yourPizza = toppings[i].topping;
     if (yourPizza === this.topping) {
       cost += toppings[i].price;
-    }
-  }
-  for (let i = 0;  i < toppings2.length; i++) {
-    const yourPizza = toppings2[i].topping2;
-    if (yourPizza === this.topping2) {
-      cost += toppings2[i].price;
-    }
-  }
-  for (let i = 0;  i < toppings3.length; i++) {
-    const yourPizza = toppings3[i].topping3;
-    if (yourPizza === this.topping3) {
-      cost += toppings3[i].price;
-    }
-  }
-  for (let i = 0;  i < toppings4.length; i++) {
-    const yourPizza = toppings4[i].topping4;
-    if (yourPizza === this.topping4) {
-      cost += toppings4[i].price;
     }
   }
     return cost;
@@ -107,13 +64,8 @@ $(document).ready(function() {
     $("#order").hide();
     const inputName = $("input#custName").val();
     const inputSize = $("input:radio[name=size]:checked").val();
-    // const inputSize = $("select#size").val();
     const inputTopping1 = $("select#toppings").val();
-    const inputTopping2 = $("select#toppings2").val();
-    const inputTopping3 = $("select#toppings3").val();
-    const inputTopping4 = $("select#toppings4").val();
-    const friendPizza = new Pizza(inputSize, inputTopping1, inputTopping2, inputTopping3, inputTopping4);
-    // let yourYums = ((friendPizza.toppingNames()).toString());
+    const friendPizza = new Pizza(inputSize, inputTopping1);
     let yourYums = ((friendPizza.toppingNames()).join(", "));
     let yourPrice = (friendPizza.theDamage() + friendPizza.toppingTotal());
     console.log(friendPizza.theDamage(), "The Damage");
@@ -121,9 +73,10 @@ $(document).ready(function() {
     console.log(yourPrice, "Your Price");
     $("#customerName").text(inputName);
     $("#pSize").html(friendPizza.size);
+    $("#sauce").text(friendPizza.topping);
     $("#toppingList").html(yourYums);
     $("#finalPrice").html("$" + yourPrice);
-    console.log(friendPizza.toppingNames());
+    console.log(friendPizza.topping, "toppings");
   });
 
 
